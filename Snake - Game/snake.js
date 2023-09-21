27e84e89130d967828a87b4c9b84836781a8dd06
mahderp <mahderp1@gmail.com>
2023-09-21 06:03:10 +0300

    the ~Snake~ Game

---
+++ Snake - Game/snake.js
@@ -0,0 +1,68 @@
+
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0;
+var velocityY = 0;
+
+//food
+var foodX;
+var foodY;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height =  rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d"); //used for drawing on the board
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    update();
+}
+
+function update() {
+    context.fillStyle="black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="lime";
+    snakeX += velocityX;
+    snakeY += velocityY;
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
+
+    context.fillStyle="red";
+    context.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+function changeDirection(e) {
+    if (e.code == "ArrowUp") {
+        velocityX = 0;
+        velocityY = -1;
+    }
+    else if (e.code == "ArrowDown") {
+        velocityX = 0;
+        velocityY = 1;
+    }
+    else if (e.code == "ArrowLeft") {
+        velocityX = -1;
+        velocityY = 0;
+    }
+    else if (e.code == "ArrowRight") {
+        velocityX = 1;
+        velocityY = 0;
+    }
+}
+
+
+function placeFood() {
+    //0-1) *cols -> (0-19.9999) -> (0-19) * 25
+    foodX = Math.floor(Math.random() * cols) * blockSize;
+    foodY = Math.floor(Math.random() * rows) * blockSize;
+}
